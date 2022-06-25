19a1d7d800824aa4781c799449c78a574dab5fbd
Olt Latifi <oltlatifi2003@gmail.com>
2022-06-25 13:25:31 +0200

    Pagination set to 100

--- core/settings.py
+++ core/settings.py
@@ -80,7 +80,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS':'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 AUTH_USER_MODEL = "users.NewUser"
